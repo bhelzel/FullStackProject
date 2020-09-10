97b09cd25a77a0ec338c509bbf52b5fc5fda7c69
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-09-10 10:13:13 -0700

    update db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_214632) do
+ActiveRecord::Schema.define(version: 2020_09_10_170624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,8 +41,6 @@ ActiveRecord::Schema.define(version: 2020_01_16_214632) do
     t.integer "recipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["recipe_id"], name: "index_likes_on_recipe_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
